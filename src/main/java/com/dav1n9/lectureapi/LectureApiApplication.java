f68e50ec88cb25af38155202129be582fa11f668
Davin <85967571+dav1n9@users.noreply.github.com>
2024-07-11 17:03:29 +0900

    init commit

---
+++ src/main/java/com/dav1n9/lectureapi/LectureApiApplication.java
@@ -0,0 +1,13 @@
+package com.dav1n9.lectureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dav1n9/lectureapi/LectureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dav1n9.lectureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
