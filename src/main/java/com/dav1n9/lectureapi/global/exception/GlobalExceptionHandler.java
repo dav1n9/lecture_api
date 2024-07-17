6570d513f474f8c211d4c7339f0604067c924239
Davin <85967571+dav1n9@users.noreply.github.com>
2024-07-17 15:49:14 +0900

    fix: status 함께 반환하기 위해 반환 타입 변경

--- src/main/java/com/dav1n9/lectureapi/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dav1n9/lectureapi/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.dav1n9.lectureapi.global.exception;
 
 import com.dav1n9.lectureapi.global.api.ApiResponse;
 import com.dav1n9.lectureapi.global.api.ApiUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
-    public ApiResponse<ErrorResponse> handleIllegalArgumentException (IllegalArgumentException ex) {
-        return ApiUtils.ERROR(ex.getMessage(), null);
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiUtils.ERROR(ex.getMessage(), null));
     }
+
     @ExceptionHandler(NullPointerException.class)
-    public ApiResponse<ErrorResponse> handleNullPointerException (NullPointerException ex) {
-        return ApiUtils.ERROR(ex.getMessage(), null);
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleNullPointerException(NullPointerException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiUtils.ERROR(ex.getMessage(), null));
     }
 }
